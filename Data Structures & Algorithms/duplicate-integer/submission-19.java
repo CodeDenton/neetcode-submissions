5cb9c550f9047cd3907b66c630436bedae7f5789
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-07-06 08:20:57 -0700

    Add: duplicate-integer - submission-19

---
+++ Data Structures & Algorithms/duplicate-integer/submission-19.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashSet<Integer> dupSet = new HashSet<>();
+
+        for(int i=0; i<nums.length; i++) {
+            if(dupSet.contains(nums[i])) return true;
+            dupSet.add(nums[i]);
+        }
+        return false;
+    }
+}
