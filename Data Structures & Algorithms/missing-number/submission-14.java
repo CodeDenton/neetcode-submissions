7be8ddc5964d91220393c388cddde3108956870a
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-07-08 20:13:43 -0700

    Add: missing-number - submission-14

---
+++ Data Structures & Algorithms/missing-number/submission-14.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        // 0 + 1 + 2 + 3 = 6
+        // 0 + 1 + 3 = 4
+        //6 - 4 = 2
+        int x = 0;
+        for(int i=0; i<=nums.length; i++) {
+            x += i;
+        }
+        int y = 0;
+        for(int i=0; i<nums.length; i++) {
+            y += nums[i];
+        }
+        return x-y;
+    }
+}
