5a7cae3e31c03c9e1f35e9ac50a8a7f6389de32e
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-07-22 13:21:34 -0700

    Add: is-palindrome - submission-11

---
+++ Data Structures & Algorithms/is-palindrome/submission-11.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        StringBuilder sBuild = new StringBuilder();
+        char[] sChar = s.toCharArray();
+        for(char c : sChar) {
+            if(Character.isLetterOrDigit(c)) {
+                sBuild.append(Character.toLowerCase(c));
+            }
+        }
+        return sBuild.toString().equals(sBuild.reverse().toString());
+    }
+}
