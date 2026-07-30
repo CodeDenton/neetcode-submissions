c4becf6c6dd917c268e1def1baa60dd24af899a0
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-07-30 08:46:32 -0700

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, ArrayList<String>> keyMap = new HashMap<>();
+
+        for(String s : strs) {
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            String sortedS = new String(c);
+            keyMap.putIfAbsent(sortedS, new ArrayList<>());
+            keyMap.get(sortedS).add(s);
+        }
+
+        return new ArrayList<>(keyMap.values());
+    }
+}
