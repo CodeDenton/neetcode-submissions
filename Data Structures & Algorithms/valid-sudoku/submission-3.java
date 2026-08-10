e677c1d7d140ddfa0490a5097b8e289b2562350a
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-08-09 21:59:07 -0700

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        Set<String> seen = new HashSet<>();
+        for(int r=0; r<9; r++) {
+            for(int c=0; c<9; c++) {
+                char val = board[r][c];
+                if(val == '.') continue;
+
+                String rowTot = "Row" + r + '-' + val;
+                String colTot = "Col" + c + '-' + val;
+                String boxTot = "Box" + (r/3) + '-' + (c/3) + '-' + val;
+
+                if(seen.contains(rowTot) || seen.contains(colTot) || seen.contains(boxTot)) {
+                    return false;
+                }
+
+                seen.add(rowTot);
+                seen.add(colTot);
+                seen.add(boxTot);
+            }
+        }
+        return true;
+    }
+}
