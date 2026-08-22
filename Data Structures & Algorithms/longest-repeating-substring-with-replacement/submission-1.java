8337a497545ff12cd45cea3c28cee14b6ca419c5
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-08-21 17:08:27 -0700

    Add: longest-repeating-substring-with-replacement - submission-1

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-1.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int result = 0;
+        Set<Character> charSet = new HashSet<>();
+        for(char c : s.toCharArray()) {
+            charSet.add(c);
+        }
+        //EKDIDDDK
+        //charset = EKDIK
+
+        for(char c : charSet) {
+            int count = 0;
+            int left = 0;
+            for(int right = 0; right < s.length(); right++) {
+                if(s.charAt(right) == c) {
+                    count++;
+                }
+
+                while((right - left + 1) - count > k) {
+                    if(s.charAt(left) == c) {
+                        count--;
+                    }
+                    left++;
+                }
+                result = Math.max(result, right - left + 1);
+            }
+        }
+
+        return result;
+    }
+}
