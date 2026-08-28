c3ed32312226c83952764765943e59d8e7d370c6
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-08-27 23:11:39 -0700

    Add: longest-repeating-substring-with-replacement - submission-11

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-11.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int result = 0;
+        Set<Character> set = new HashSet<>();
+        for(char c : s.toCharArray()) {
+            set.add(c);
+        }
+
+        for(char c : set) {
+            int left = 0;
+            int count = 0;
+            for(int right=0; right < s.length(); right++) {
+                if(s.charAt(right) == c) {
+                    count++;
+                }
+                while((right - left + 1) - k > count ) {
+                    if(s.charAt(left) == c) {
+                        count--;
+                    }
+                    left++;
+                }
+                result = Math.max(result, right - left + 1);
+            }
+        }
+        return result;
+    }
+}
