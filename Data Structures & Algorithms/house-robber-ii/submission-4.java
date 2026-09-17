8115969cb03d5caece9197979587901813ac2f06
Denton Manders <154308667+CodeDenton@users.noreply.github.com>
2026-09-17 16:04:07 -0700

    Add: house-robber-ii - submission-4

---
+++ Data Structures & Algorithms/house-robber-ii/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(nums.length == 1) {
+            return nums[0];
+        }
+        int cutLast = robHouses(Arrays.copyOfRange(nums, 0, n-1));
+        int cutFirst = robHouses(Arrays.copyOfRange(nums,1, n));
+        return Math.max(cutLast, cutFirst);
+    }
+
+    public int robHouses(int [] nums) {
+        int rob1 = 0;
+        int rob2 = 0;
+        for(int n : nums) {
+            int temp = Math.max(n + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        return rob2;
+    }
+}
